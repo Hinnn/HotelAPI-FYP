4755582f8af3986cd75dfab18b2abfdd574af2f3
Yue Wang <hin@Yue-Wang.local>
2019-04-23 12:49:24 +0100

    add test and paypal

---
+++ test/routers/booking-test.js
@@ -0,0 +1,6 @@
+let chai= require('chai');
+let chaiHttp = require('chai-http');
+let things = require('chai-things');
+let mongoose = require('mongoose');
+let bcrypt = require('bcrypt-nodejs');
+let _ = require('lodash');
